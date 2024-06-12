26d55b6765a3cc5a29d7234aab5f68f7f3c02532
Daria <sigulia39@gmail.com>
2024-06-12 14:06:39 +0300

    В тест test_post_v1_account добавлен метод с активацией пользователя

--- tests/test_post_v1_account.py
+++ tests/test_post_v1_account.py
@@ -3,13 +3,18 @@ from services.dm_api_account import DmApiAccount
 
 def test_post_v1_account():
     api = DmApiAccount(host='http://5.63.153.31:5051')
+    token = '118401c5-3cce-4b36-94b4-5aa3e21651f6'
     json = {
         "login": "User777",
         "email": "Postman_user777@gmail.com",
         "password": "resu4321"
     }
 
-    response = api.account.post_v1_account(
+    response_account = api.account.post_v1_account(
         json=json
     )
-    print(response)
+    response_token = api.account.put_v1_account_token(
+        token=token
+    )
+    print(response_account)
+    print(response_token)
