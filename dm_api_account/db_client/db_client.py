708bf070a290d124cc65448f8e83d0ecdedefea9
Daria <sigulia39@gmail.com>
2024-07-30 00:00:15 +0300

    Добавлено подключение к бд, дописан тест test_post_v1_account

---
+++ dm_api_account/db_client/db_client.py
@@ -0,0 +1,11 @@
+import uuid
+import records
+import structlog
+
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(indent=4, sort_keys=True, ensure_ascii=False)
+    ]
+)
+
+
--- tests/test_post_v1_account.py
+++ tests/test_post_v1_account.py
@@ -1,3 +1,5 @@
+import time
+from generic.helpers.dm_db import DmDatabase
 from services import *
 from dm_api_account.models.user_envelope import Roles
 from hamcrest import assert_that, has_properties
@@ -5,16 +7,32 @@ from hamcrest import assert_that, has_properties
 
 def test_post_v1_account():
     api = Facade(host='http://5.63.153.31:5051')
-    login = "Cat83"
-    email = "Kitty83@gmail.com"
+    db = DmDatabase(user='postgres', password='admin', host='5.63.153.31', database='dm3.5')
+    login = "Cat"
+    email = "Kitty111@gmail.com"
     password = "meowmeow"
 
+    try:
+        dataset_delete = db.delete_user_by_login(login=login)
+        assert len(dataset_delete) == 0
+    except:
+        pass
     api.account.register_new_user(
         login=login,
         email=email,
         password=password
     )
+    dataset = db.get_user_by_user(login=login)
+    for row in dataset:
+        assert row['Login'] == login, f"User {login} is not found"
+        assert row['Activated'] is False, f"User {login} is not activated"
+
     response_token = api.account.activate_registered_user(login=login)
+    time.sleep(2)
+    dataset2 = db.get_user_by_user(login=login)
+    for row in dataset2:
+        assert row['Activated'] is True, f"User {login} is not activated"
+
     assert_that(response_token.resource, has_properties(
         {
             "login": login,
